ae35d1c5de41fd4f5c423de0fdd3de2b9d497a2f
Akhilesh <akhileshallewar880@gmail.com>
2024-01-16 14:33:15 +0530

    dotnet app skeleton ready

---
+++ Server/API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // api/users
+public class  UsersController : ControllerBase
+{
+    private readonly DataContext context;
+    public UsersController(DataContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await this.context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id}")] //api/users/1
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await this.context.Users.FindAsync();
+
+        return user;
+    }
+}
