50ce7fc04782d4ad632184507083b1e6f6f4787a
Akhilesh <akhileshallewar880@gmail.com>
2024-02-02 12:35:09 +0530

    added api middleware and interceptor

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,6 +14,7 @@ import { MemberDetailsComponent } from './members/member-details/member-details.
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { SharedModule } from './modules/shared.module';
+import { ErrorsInterceptor } from './interceptor/errors.interceptor';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,9 @@ import { SharedModule } from './modules/shared.module';
     NgbModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    {provide : HTTP_INTERCEPTORS, useClass : ErrorsInterceptor, multi : true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
